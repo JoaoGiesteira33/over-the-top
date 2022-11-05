ae0cea167d5fd507ec7020ef7662eeb79c659960
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2022-11-05 17:56:22 +0000

    TCP

--- oNode/oNode.py
+++ oNode/oNode.py
@@ -1,45 +1,63 @@
-# importing modules for the chat appimport socket
 import time
 import threading
 import sys
 import socket
 
-# AF_INET = Network Address Family : IPv4
-# SOCK_DGRAM = DataGram Socket : UDP# Function for receiving
+if(len(sys.argv) != 3):
+    print('oNode.py <bootstrapper> <config_file>')
+    sys.exit(2)
+
+bootstrapper = sys.argv[1]
+config_file = sys.argv[2]
+
+print("Bootstrapper:",bootstrapper)
+print("Config File:",config_file)
+
+DEFAULT_PORT = 8080
+BUFFER_SIZE = 20
+vizinhos = []
+
 def receiver():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind((ip_sender, port_sender)) #binding the IP address and port number
-    while True:
-        msg = s.recvfrom(1024)
-        print("\n"+msg[0].decode())
-        if "exit" in msg[0].decode() or "bye" in msg[0].decode():
-            sys.exit()
-            
-#Function for sending
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(bootstrapper, DEFAULT_PORT)
+    s.listen(1)
+
+    conn, addr = s.accept()
+    print('Connection address:', addr)
+    while 1:
+        data = conn.recv(BUFFER_SIZE)
+        if not data: break
+        print("received data:", data)
+        conn.close()
+
 def sender():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #Create socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(bootstrapper,DEFAULT_PORT)
+    #Send message
     text = "hello"
     while True:
         if "bye" in text or "exit" in text or "finish" in text:
+            s.close()
             exit()
         else:
             text = input(f'{name}:')
             text = name+":"+text
-            s.sendto(text.encode(), (ip_receiver, port_receiver))
+            s.send(text.encode())
             
 print("Initializing....")
             
-ip_receiver = input("\nEnter the IP of reciever: ")
-port_receiver = int(input("\nEnter the port of the reciever: "))
+#ip_receiver = input("\nEnter the IP of reciever: ")
+#port_receiver = int(input("\nEnter the port of the reciever: "))
 
-ip_sender = input("\nEnter the IP of your system : ")
-port_sender = int(input("\nEnter the port of your system: "))
+#ip_sender = input("\nEnter the IP of your system : ")
+#port_sender = int(input("\nEnter the port of your system: "))
 
 name = input("Enter your name: ")
 
-print("Waiting for client....")
-time.sleep(1)
-print("Connection established....")
+#print("Waiting for client....")
+#time.sleep(1)
+#print("Connection established....")
 
 # Using Multi-threading
 send = threading.Thread(target=sender)
