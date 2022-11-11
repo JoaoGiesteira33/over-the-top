52510f93624ccdf6bf90fcaef7b2293582ae4fdf
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2022-11-11 23:58:38 +0000

    Fix class

--- oNode/oNode.java
+++ oNode/oNode.java
@@ -4,6 +4,28 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
+public class oNode{
+    public static void main(String[] args){
+        if(args.length != 1){
+            System.out.println("oNode.java <bootstrapper>");
+            return;
+        }
+
+        String bootstrapper = args[0];
+        System.out.println("Bootstrapper: " + bootstrapper);
+
+        Server s = new Server();
+        Thread serverThread = new Thread(s);
+        serverThread.start();
+
+        if(!bootstrapper.equals("Server")){
+            Client c = new Client(bootstrapper);
+            Thread clientThread = new Thread(c);
+            clientThread.start();
+        }
+    }
+}
+
 class Client implements Runnable{ 
     String ipAddress;
     
@@ -104,24 +126,3 @@ class Server implements Runnable{
     }
 }
 
-public class oNode{
-    public static void main(String[] args){
-        if(args.length != 1){
-            System.out.println("oNode.java <bootstrapper>");
-            return;
-        }
-
-        String bootstrapper = args[0];
-        System.out.println("Bootstrapper: " + bootstrapper);
-
-        Server s = new Server();
-        Thread serverThread = new Thread(s);
-        serverThread.start();
-
-        if(!bootstrapper.equals("Server")){
-            Client c = new Client(bootstrapper);
-            Thread clientThread = new Thread(c);
-            clientThread.start();
-        }
-    }
-}
